c2356a6b9a15033d8ec0eb973c4e7dc324b4f522
sabihadudhia <sabiha.dudhia@iu-study.org>
2024-11-20 05:47:30 +0200

    Delete Terraform Project directory

--- Terraform Project/provider.tf
+++
@@ -1,4 +0,0 @@
-# Provider configuration to specify the AWS region where resources will be created
-provider "aws" {
-  region = "us-east-1"  						# Use the US East (N. Virginia) region for AWS resources
-}
